1f5c6ea5cf8b9f53e77648ab3f066f55219f2d89
Simon Chalder <66743889+simonchalder@users.noreply.github.com>
2022-11-06 15:46:44 +0000

    New Commit
    V1.1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import customtkinter
 import itertools
 import tkinter
@@ -5,66 +6,44 @@ from tkinter import filedialog
 import pandas as pd
 import numpy
 
+newFiles = None
+filePath1 = None
+filePath2 = None
+csvFileData1 = None
+csvFileData2 = None
+axisNames1 = None
+xAxisName1 = None
+yAxisName1 = None
+xAxisName2 = None
+yAxisName2 = None
+xAxisList1 = []
+yAxisList1 = []
+xAxisList2 = []
+yAxisList2 = []
+
+
 class file:
 
-    def csv1Read(file):
-        csvFile1 = pd.read_csv(file) # Place the contents of file 1 into a variable
-        return csvFile1
-
-    def csv2Read(file):
-        csvFile2 = pd.read_csv(file) # Place the contents of file 1 into a variable
-        return csvFile2
-    
-    def button1SelFile():
-        filename1 = filedialog.askopenfilename(initialdir = "C:\\", title = "Select a File", filetypes = (("CSV Files", "*.csv*"), ("all files", "*.*")))
-        labelFile1.configure(text=filename1)
-        csvFile = file.csv1Read(filename1)
-        file.findAxisFile1(csvFile)
-
-    def button2SelFile():
-        filename2 = filedialog.askopenfilename(initialdir = "C:\\", title = "Select a File", filetypes = (("CSV Files", "*.csv*"), ("all files", "*.*")))
-        labelFile2.configure(text=filename2)
-        csvFile = file.csv2Read(filename2)
-        file.findAxisFile2(csvFile)
-
-    def findAxisFile1(csvFile):
-        axisNames = csvFile.columns
-        xaxis1Name = axisNames[0]
-        yaxis1Name = axisNames[1]
-        plot1XAxisBox.insert(0, xaxis1Name)
-        plot1YAxisBox.insert(0, yaxis1Name)
-        
-    def findAxisFile2(csvFile):
-        axisNames = csvFile.columns
-        xaxis1Name = axisNames[0]
-        yaxis1Name = axisNames[1]
-        plot2XAxisBox.insert(0, xaxis1Name)
-        plot2YAxisBox.insert(0, yaxis1Name)
-
-    def csvAxisToList1(self, file):
-        csvFile1 = pd.read_csv(self, file) # Place the contents of file 1 into a variable
-        month = csvFile1['Date'].tolist() # Extract 1st column data into a list
-        pop = csvFile1['Population'].tolist()
-        return month, pop
-
-    def csvAxisToList2(self, file):
-        csvFile1 = pd.read_csv(file) # Place the contents of file 1 into a variable
-        month = csvFile1['Date'].tolist() # Extract 1st column data into a list
-        pop = csvFile1['Population'].tolist()
-        return month, pop
-
+    def __init__(self, filePath1, filePath2, csvFileData1, csvFileData2, xAxisName1, yAxisName1, xAxisName2, yAxisName2, xAxisList1, yAxisList1, xAxisList2, yAxisList2):
+        self.filePath1 = filePath1
+        self.filePath2 = filePath2
+        self.csvFileData1 = csvFileData1
+        self.csvFileData2 = csvFileData2
+        self.xAxisName1 = xAxisName1
+        self.yAxisName1 = yAxisName1
+        self.xAxisName2 = xAxisName2
+        self.yAxisName2 = yAxisName2
+        self.xAxisList1 = xAxisList1
+        self.yAxisList1 = yAxisList1
+        self.xAxisList2 = xAxisList2
+        self.yAxisList2 = yAxisList2
 
 class calc:
-
-    def combobox_callback(choice):
-        print("combobox dropdown clicked:", choice)
 
     def population_change(pop):
         pop_change = numpy.diff(pop)
         pop_change = [float(x) for x in pop_change]
         return pop_change
-
-# Function to Calculate % growth rate in population --------------------------------------------------------------------------------------
 
     def growth_rate(pop):
         rate = pd.Series(pop)
@@ -82,7 +61,95 @@ class calc:
             except ZeroDivisionError:           
                 K = C[-1]
                 C.append(K)
-        return(C)   
+        return(C)
+        
+
+def makeNewFilesObject(file1, file2, csv1, csv2, x1, y1, x2, y2, xl1, yl1, xl2, yl2):
+    newFiles = file(file1, file2, csv1, csv2, x1, y1, x2, y2, xl1, yl1, xl2, yl2)
+    return newFiles
+
+def printCheck():
+    print(newFiles.xAxisName1)
+
+def fileButton1():
+    global newFiles, filePath1, filePath2, csvFileData1, xAxisName1, yAxisName1, xAxisList1, yAxisList1, csvFileData2, xAxisName2, yAxisName2, xAxisList2, yAxisList2
+    filePath1 = filedialog.askopenfilename(initialdir = "C:\\", title = "Select File 1", filetypes = (("CSV Files", "*.csv*"), ("all files", "*.*")))   
+    csvFileData1 = pd.read_csv(filePath1)
+    axisNames1 = csvFileData1.columns
+
+    xAxisName1 = axisNames1[0]
+    yAxisName1 = axisNames1[1]
+
+    xAxisList1 = csvFileData1[xAxisName1]
+    yAxisList1 = csvFileData1[yAxisName1]
+    
+    filePath2 = filedialog.askopenfilename(initialdir = "C:\\", title = "Select File 2", filetypes = (("CSV Files", "*.csv*"), ("all files", "*.*")))   
+    csvFileData2 = pd.read_csv(filePath2)
+    axisNames2 = csvFileData2.columns
+    
+    xAxisName2 = axisNames2[0]
+    yAxisName2 = axisNames2[1]
+
+    xAxisList2 = csvFileData2[xAxisName2]
+    yAxisList2 = csvFileData2[yAxisName2]
+    
+    newFiles = makeNewFilesObject(filePath1, filePath2, csvFileData1, csvFileData2, xAxisName1, yAxisName1, xAxisName2, yAxisName2, xAxisList1, yAxisList1, xAxisList2, yAxisList2)
+    
+    labelFile1.configure(text=newFiles.filePath1)
+    labelFile2.configure(text=newFiles.filePath2)
+    plot1XAxisBox.insert(0, newFiles.xAxisName1)
+    plot1YAxisBox.insert(0, newFiles.yAxisName1)
+    plot2XAxisBox.insert(0, newFiles.xAxisName2)
+    plot2YAxisBox.insert(0, newFiles.yAxisName2)
+
+cc1 = None
+cc2 = None
+
+def genGraph():
+    if combobox.get() == "Carrying Capacity":
+        pop1 = newFiles.yAxisList1
+        pc1 = calc.population_change(pop1)
+        lastpcval1 = pc1[-1] # get the last value in the list
+        pc1.append(lastpcval1)
+        gr1 = calc.growth_rate(pop1)
+        lastgrval1 = gr1[-1] # Get the last value in the list
+        gr1.append(lastgrval1)
+        cc1 = calc.calc_car_cap(gr1, pop1, pc1)
+
+        pop2 = newFiles.yAxisList2
+        pc2 = calc.population_change(pop2)
+        lastpcval2 = pc2[-1] # get the last value in the list
+        pc2.append(lastpcval2)
+        gr2 = calc.growth_rate(pop2)
+        lastgrval2 = gr2[-1] # Get the last value in the list
+        gr2.append(lastgrval2)
+        cc2 = calc.calc_car_cap(gr2, pop2, pc2)
+        
+        if titleEntry.get() == '':
+            plt.title(entryFile1.get() + " vs " + entryFile2.get())
+        else:
+            title = titleEntry.get()
+            plt.title(title)
+
+        plt.grid(True)
+        
+        plt.plot(newFiles.xAxisList1,newFiles.yAxisList1, label = entryFile1.get(), color = "black") # Plot data from file 1 population numbers       
+        
+        plt.plot(newFiles.xAxisList1, cc1, label = entryFile1.get() + " Carrying Capacity", color = "blue", linestyle = 'dotted') # Plot carrying capacity data for species 1
+       
+        plt.plot(newFiles.xAxisList2,newFiles.yAxisList2, label = entryFile2.get(), color = "green") # Plot data from file 2 population numbers       
+        
+        plt.plot(newFiles.xAxisList2, cc2, label = entryFile2.get() + " Carrying Capacity", color = "red", linestyle = 'dotted') # Plot carrying capacity data for species 1
+        
+        plt.xlabel(newFiles.xAxisName1) # Axis labels # Axis labels
+        plt.ylabel(newFiles.yAxisName2)
+
+        plt.legend(loc="upper right") # Display graph legend
+
+        plt.show() 
+
+    else:
+        pass
 
 app = customtkinter.CTk()
 app.geometry(f"{1120}x{500}")
@@ -90,32 +157,25 @@ app.title("New Graph")
 
 customtkinter.set_appearance_mode("dark")
 
-newFile = file
-newCalc = calc
-
 # Frame 1 ------------------------------------------------------------------------------------------------------
 
 frame1 = customtkinter.CTkFrame(app, width=550, height=100, corner_radius=10)
 frame1.grid(column=0, row=0, padx=20, pady=10)
 
-file1Button = customtkinter.CTkButton(master=frame1, text="Select File 1", command=newFile.button1SelFile)
+file1Button = customtkinter.CTkButton(master=frame1, text="Select Files To Compare", command=fileButton1)
 file1Button.grid(column=0, row=0, padx=60, pady=20, sticky="w")
 
-entryFile1 = customtkinter.CTkEntry(master=frame1, placeholder_text="Plot Name")
+entryFile1 = customtkinter.CTkEntry(master=frame1, placeholder_text="Plot 1 Name")
 entryFile1.grid(column=1, row=0, padx=60, pady=10, sticky="e")
 
 labelFile1 = customtkinter.CTkLabel(master=frame1, text="")
 labelFile1.grid(column=0, row=1, columnspan=2)
 
-
-file2Button = customtkinter.CTkButton(master=frame1, text="Select File 2", command=newFile.button2SelFile)
-file2Button.grid(column=0, row=2)
-
-entryFile2 = customtkinter.CTkEntry(master=frame1, placeholder_text="Plot Name")
-entryFile2.grid(column=1, row=2, padx=20, pady=10)
+entryFile2 = customtkinter.CTkEntry(master=frame1, placeholder_text="Plot 2 Name")
+entryFile2.grid(column=1, row=3, padx=20, pady=10)
 
 labelFile2 = customtkinter.CTkLabel(master=frame1, text="")
-labelFile2.grid(column=0, row=3, columnspan=2)
+labelFile2.grid(column=0, row=4, columnspan=2)
 
 # -------------------------------------------------------------------------------------------------------------
 
@@ -170,17 +230,17 @@ frame4.grid(column=1, row=0, padx=20, pady=10, ipadx=40, ipady=25)
 calcLabel = customtkinter.CTkLabel(master=frame4, text="Select Calculation To Display")
 calcLabel.grid(column=0, row=0, padx=10, pady=20)
 
-combobox = customtkinter.CTkComboBox(master=frame4, values=["Carrying Capacity", "Correlation Coefficient", "Standard Deviation + Mean"], command=calc.combobox_callback)
+combobox = customtkinter.CTkComboBox(master=frame4, values=["None", "Carrying Capacity", "Correlation Coefficient", "Standard Deviation + Mean"], command=None)
 combobox.grid(column=0, row=1, padx=60, pady=10, ipadx=30)
-combobox.set("Carrying Capacity")  # set initial value
+combobox.set("None")  # set initial value
 
 # Frame 5 --------------------------------------------------------------------------------------------
 
 frame5 = customtkinter.CTkFrame(app, width=550, height=100, corner_radius=10)
 frame5.grid(column=1, row=1, padx=20, pady=10, ipady=25)
 
-# genGraphButton = customtkinter.CTkButton(master=frame1, text="Select File 2", command=newGraph.genGraph)
-# genGraphButton.grid(column=0, row=2)
+genGraphButton = customtkinter.CTkButton(master=frame5, text="Generate Graph", command=genGraph)
+genGraphButton.grid(column=0, row=2)
 
 app.mainloop()
 
